bcc4dfed73af2834094c01333336e5171396ae4a
Hunter Larco <hunter@larcolabs.com>
2025-09-27 10:26:08 -0400

    tests work

--- src/cli/shared/parsePathAliases.ts
+++ src/cli/shared/parsePathAliases.ts
@@ -9,13 +9,13 @@ export const parsePathAliases = (
     const parts = encodedAlias.split(`=`);
     if (parts.length !== 2) {
       throw new Error(
-        `Expected an alias in the form "alias=source" but found ${encodedAlias}`,
+        `Expected an alias in the form "alias=source" but found "${encodedAlias}"`,
       );
     }
 
     const [alias, source] = parts;
     if (decodedAliases.has(alias)) {
-      throw new Error(`Alias ${alias} is set multiple times.`);
+      throw new Error(`Alias "${alias}" is set multiple times.`);
     }
 
     decodedAliases.set(alias, source);
